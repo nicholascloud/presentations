d50c85a7c79c3396c8702a7a378db0b55ccc4ce0
nicholascloud <me@nicholascloud.com>
2011-10-30 21:19:52 -0500

    removed nuget task

--- 20111026 Rake/Tasks/nuget.rb
+++
@@ -1,9 +0,0 @@
-namespace :nuget do
-  
-  nugetpack :pack do |nuget|
-    nuget.command = './GrokMob/packages/NuGet.CommandLine.1.5.21005.9019/tools/NuGet.exe'
-    nuget.nuspec = './GrokMob/GrokMob.Pester/Package.nuspec'
-    nuget.basefolder = './GrokMob/GrokMob.Pester'
-    nuget.output = './Pack'
-  end
-end
