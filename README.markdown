6b0b219d98aa326cc4ce75beca4785465aa54bee
nicholascloud <me@nicholascloud.com>
2012-01-18 19:02:08 -0600

    renamed presentation folders

--- README.markdown
+++ README.markdown
@@ -4,16 +4,16 @@
 
 ### Presentations: ###
 
-- 10/20/2010 Introduction to NHibernate
-- 11/17/2010 Functional Programming with Scheme
-- 12/15/2010 Introduction to F#
-- 01/19/2011 HTML5, CSS3, and JavaScript
-- 02/23/2011 Git and Other Marvels
-- 03/23/2011 Threading in C#
-- 04/27/2011 .NET Micro Web Frameworks (Nancy and Jessica)
+- 10/20/2010 Introduction to NHibernate (Nicholas Cloud)
+- 11/17/2010 Functional Programming with Scheme (Aaron Hsu)
+- 12/15/2010 Introduction to F# (Nicholas Cloud)
+- 01/19/2011 HTML5, CSS3, and JavaScript (Nicholas Cloud)
+- 02/23/2011 Git and Other Marvels (Mark Borcherding)
+- 03/23/2011 Threading in C# (Nicholas Cloud)
+- 04/27/2011 .NET Micro Web Frameworks: Nancy and Jessica (Nicholas Cloud)
 - 05/25/2011 (meeting canceled; notes on ReSharper)
-- 06/22/2011 DLR Dynamic Programming
-- 06/22/2011 Impromptu Interface
-- 06/22/2011 Hacker News Radio
-- 08/24/2011 CQRS and Event Sourcing with NCQRS
-- 08/24/2011 ReSharper Structured Patterns
+- 06/22/2011 DLR Dynamic Programming (Nicholas Cloud)
+- 06/22/2011 Hacker News Radio (Jeff Sigmon) and Impromptu Interface (Jay Tuley) 
+- 08/24/2011 CQRS and Event Sourcing with NCQRS (Nicholas Cloud) and ReSharper Structured Patterns (Josh Buedel)
+- 11/02/2011 Using Rake to Make Your Builds Majestic (Nicholas Cloud)
+- 11/30/2011 Introduction to CoffeeScript (Nicholas Cloud) and Hubot (Mark Borcherding)
---
+++ rake/GrokMob/GrokMob.SchemaMigration/Migration1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator;
+
+namespace GrokMob.SchemaMigration {
+  [Migration(1)]
+  public class Migration1 : Migration {
+    public override void Up() {
+
+      // MEMBER -----------------------------------------------------------------------------------
+
+      Create.Table("Member")
+        .WithColumn("Handle").AsString().NotNullable().PrimaryKey("PK_Member.Handle")
+        .WithColumn("Name").AsString().NotNullable()
+        .WithColumn("Technologies").AsString()
+        .WithColumn("JoinedAt").AsDateTime().NotNullable();
+
+      Insert.IntoTable("Member")
+        .Row(new {
+          Name = "Nicholas Cloud",
+          Handle = "ncloud",
+          Technologies = ".NET, HTML5, JavaScript",
+          JoinedAt = DateTime.Now.AddYears(-1)
+        })
+        .Row(new {
+          Name = "Han Solo",
+          Handle = "nerfherder",
+          Technologies = "blaster, Millennium Falcon",
+          JoinedAt = DateTime.Now.AddMonths(-3)
+        })
+        .Row(new {
+          Name = "Fry",
+          Handle = "fry",
+          Technologies = "pizza, Planet Express ship, Bender",
+          JoinedAt = DateTime.Now.AddHours(-2)
+        })
+        .Row(new {
+          Name = "Bender",
+          Handle = "lordbender",
+          Technologies = "my shiny metal ass",
+          JoinedAt = DateTime.Now.AddHours(-1)
+        })
+        .Row(new {
+          Name = "Bruce Wayne",
+          Handle = "Batman",
+          Technologies = "Batarang, Batmobile, Shark Repellant",
+          JoinedAt = DateTime.Now.AddMonths(-3)
+        });
+
+      // MEETING ----------------------------------------------------------------------------------
+
+      Create.Table("Meeting")
+        .WithColumn("Id").AsInt32().NotNullable().PrimaryKey("PK_Meeting.Id").Identity()
+        .WithColumn("Title").AsString().NotNullable()
+        .WithColumn("Description").AsString().NotNullable()
+        .WithColumn("ScheduledFor").AsDateTime().NotNullable()
+        .WithColumn("Location").AsString().NotNullable();
+
+      Execute.Sql("set IDENTITY_INSERT Meeting ON;");
+      Execute.Sql(Resource.Data("Meeting"));
+      Execute.Sql("set IDENTITY_INSERT Meeting OFF;");
+
+      // COMMENT ----------------------------------------------------------------------------------
+
+      Create.Table("Comment")
+        .WithColumn("Id").AsInt32().NotNullable().PrimaryKey("PK_Comment.Id").Identity()
+        .WithColumn("MeetingId").AsInt32().NotNullable()
+        .WithColumn("MemberHandle").AsString().NotNullable()
+        .WithColumn("Content").AsString().NotNullable()
+        .WithColumn("CreatedAt").AsDateTime().NotNullable();
+      Create.ForeignKey("FK_Comment.MeetingId-Meeting.Id")
+        .FromTable("Comment").ForeignColumn("MeetingId")
+        .ToTable("Meeting").PrimaryColumn("Id");
+      Create.ForeignKey("FK_Comment.MemberHandle-Member.Handle")
+        .FromTable("Comment").ForeignColumn("MemberHandle")
+        .ToTable("Member").PrimaryColumn("Handle");
+      Create.Index("IX_Comment.MemberHandle")
+        .OnTable("Comment").OnColumn("MemberHandle")
+        .Ascending().WithOptions().NonClustered();
+
+      Execute.Sql("set IDENTITY_INSERT Comment ON;");
+      Execute.Sql(Resource.Data("Comment"));
+      Execute.Sql("set IDENTITY_INSERT Comment OFF;");
+    }
+
+    public override void Down() {
+      Delete.Table("Comment");
+      Delete.Table("Meeting");
+      Delete.Table("Member");
+    }
+  }
+}
