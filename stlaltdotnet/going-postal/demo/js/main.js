ff003224744fcbb4da7b3e3b30eebc9c459b91d8
nicholascloud <me@nicholascloud.com>
2012-04-17 20:08:27 -0500

    finished going postal presentation

--- stlaltdotnet/going-postal/demo/js/main.js
+++ stlaltdotnet/going-postal/demo/js/main.js
@@ -1,25 +1,24 @@
 require.config({
   paths: {
-    jquery: 'jquery-1.7.2.min',
-    underscore: 'underscore-min',
-    postal: 'postal.min',
-    diagnostics: 'postal.diagnostics.min'
+    jquery: 'lib/jquery-1.7.2.min',
+    underscore: 'lib/underscore-min',
+    postal: 'lib/postal.min',
+    diagnostics: 'lib/postal.diagnostics.min'
   }
 });
 
 require(['jquery', 'postal'], function ($, postal) {
-      // Applying ignoreDuplicates to a subscription
-    var dupChannel = postal.channel("WeepingAngel.*"),
-        dupSubscription = dupChannel.subscribe(function(data) {
-                              $('<li>' + data.value + '</li>').appendTo("#example4");
-                          }).ignoreDuplicates();
-    postal.channel("WeepingAngel.DontBlink")
-          .publish({ value:"Don't Blink" });
-    postal.channel("WeepingAngel.DontBlink")
-          .publish({ value:"Don't Blink" });
-    postal.channel("WeepingAngel.DontEvenBlink")
-          .publish({ value:"Don't Even Blink" });
-    postal.channel("WeepingAngel.DontBlink")
-          .publish({ value:"Don't Close Your Eyes" });
-    dupSubscription.unsubscribe();
+var c = postal.channel('dhs.track.movement');
+c.subscribe(function (pos) {
+  document.write('Moved to ' + pos + '<br />');
+}).withThrottle(2000);
+
+//var moved = 0;
+(function move (timesMoved) {
+  if (timesMoved === 50) return;
+  c.publish(timesMoved);
+  setTimeout(function () {
+    move(timesMoved + 1);
+  }, 500);
+}(0));
 });
